ee69f9b5699c4e45104442421e5858090ed0b725
unknown <ochan@.(none)>
2009-12-31 07:37:23 -0800

    working SlidingDrawer

--- src/oc/android/widget/ResizingLayoutTest.java
+++ src/oc/android/widget/ResizingLayoutTest.java
@@ -3,8 +3,12 @@ package oc.android.widget;
 import oc.android.R;
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
+/**
+ * Test how to slide out view object
+ * @author ochan
+ *
+ */
 public class ResizingLayoutTest extends Activity
 {
 	@Override
